282372c4a62387a40a3609bc553348ac92c8c910
lucas <lucrp@users.noreply.github.com>
2023-05-02 09:54:27 +0200

    Add files via upload

---
+++ app/AnonymousClasses/ClassA.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\AnonymousClasses;
+
+
+class ClassA
+{
+    public function __construct(public int $x, public int $y)
+    {
+    }
+
+    public function foo(): string
+    {
+        return 'foo';
+    }
+
+    public function bar(): object
+    {
+        return new class($this) {
+            public function __construct(ClassA $myObj)
+            {
+                var_dump($this->x, $this->y);
+            }
+        };
+    }
+}
